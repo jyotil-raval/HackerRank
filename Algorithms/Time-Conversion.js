ede8beb25bde1c592c4c91aa6a9e31895dff52b3
jyotil-raval <jyotil@outlook.com>
2020-09-29 00:11:54 +0530

    Your Time Conversion submission got 15.00 points.

---
+++ Algorithms/Time-Conversion.js
@@ -0,0 +1,24 @@
+/*
+ * Complete the timeConversion function below.
+ */
+function timeConversion(time) {
+	if (time.includes('PM')) {
+		time = time.split('PM')[0];
+		let timeArr = time.split(':');
+		if (timeArr[0] != 12) {
+			timeArr[0] = parseInt(timeArr[0]) + 12;
+		}
+		time = timeArr.join(':');
+	} else if (time.includes('AM')) {
+		time = time.split('AM')[0];
+		let timeArr = time.split(':');
+		if (timeArr[0] == 12) {
+			timeArr[0] = '00';
+		}
+		time = timeArr.join(':');
+	}
+	console.log('timeConversion -> time', time);
+	return time;
+}
+
+timeConversion('12:40:22PM');
