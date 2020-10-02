cb9aee7c846c70b70ff6b0217644505e8757c82f
jyotil-raval <jyotil@outlook.com>
2020-10-02 13:37:46 +0530

    Day_01_Data_Type

---
+++ 30_Days_Code/Day_01_Data_Type.js
@@ -0,0 +1,12 @@
+function main() {
+	var i = 4;
+	var d = 4.0;
+	var s = 'HackerRank ';
+	var i2 = +readLine();
+	var d2 = +readLine();
+	var s2 = readLine();
+
+	console.log(i + i2);
+	console.log((d + d2).toFixed(1));
+	console.log(s + s2);
+}
