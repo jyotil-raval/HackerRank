4f673c1352a8dee695042981d2b61f0086dd50d1
jyotil-raval <jyotil@outlook.com>
2020-10-08 16:08:28 +0530

    Day 2: Compound Event Probability

---
+++ 10_days_Statistics/Day_03_Drawing_Marbles.js
@@ -0,0 +1,8 @@
+// 10 Days Statistics > Day 3: Drawing Marbles
+/**
+ * Task:
+ *  A bag contains 3 red marbles and 4 blue marbles. Then, 2 marbles are drawn from the bag, at random, without replacement. If the first marble drawn is red, what is the probability that the second marble is blue?
+ *
+ * Answer:
+ *  2/3
+ */
