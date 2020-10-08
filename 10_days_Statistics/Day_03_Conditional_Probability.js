27e482b5c3a4e8d4e455da353fd3d603a9395e2b
jyotil-raval <jyotil@outlook.com>
2020-10-08 15:47:37 +0530

    Day_03_Conditional_Probability

---
+++ 10_days_Statistics/Day_03_Conditional_Probability.js
@@ -0,0 +1,12 @@
+// 10_Days_Statistics > Day_03_Conditional_Probability
+
+/**
+ * Objective:
+ *  In this challenge, we get started with conditional probability. Check out the Tutorial tab for learning materials!
+ *
+ * Task:
+ *  Suppose a family has children, one of which is a boy. What is the probability that both children are boys?
+ *
+ * Answer:
+ *  1/3
+ */
--- 10_days_Statistics/Day_03_Conditional_Probability.md
+++
@@ -1,8 +0,0 @@
-#Objective
-In this challenge, we get started with conditional probability. Check out the Tutorial tab for learning materials!
-
-#Task
-Suppose a family has children, one of which is a boy. What is the probability that both children are boys?
-
-#Answer
-1/3
