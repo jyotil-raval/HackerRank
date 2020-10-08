e56bc88d12ba5434a6e011a226856ad57acb5209
jyotil-raval <jyotil@outlook.com>
2020-10-08 15:50:35 +0530

    Day 3: Cards of the Same Suit

---
+++ 10_days_Statistics/Day_03_Cards_of_the_Same-Suit.js
@@ -0,0 +1,9 @@
+// 10 Days Statistics > Day 3: Cards of the Same Suit
+
+/**
+ * Task:
+ *  You draw 2 cards from a standard 52-card deck without replacing them. What is the probability that both cards are of the same suit?
+ *
+ * Answer:
+ *  12/51
+ */
