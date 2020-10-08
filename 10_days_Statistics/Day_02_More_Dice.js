3e209135bcabd537bbbb1702202a3389b9beb484
jyotil-raval <jyotil@outlook.com>
2020-10-08 16:08:04 +0530

    Day 2: Basic Probability

---
+++ 10_days_Statistics/Day_02_More_Dice.js
@@ -0,0 +1,9 @@
+// 10 Days Statistics > Day 2: Basic Probability
+
+/**
+ * Task:
+ *  In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+ *
+ * Answer:
+ *  1/9
+ */
