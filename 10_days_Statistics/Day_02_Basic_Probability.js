0fdc636d9bb5b37e65929223a2e95961158660fc
jyotil-raval <jyotil@outlook.com>
2020-10-08 16:08:16 +0530

    Day 2: Basic Probability

---
+++ 10_days_Statistics/Day_02_Basic_Probability.js
@@ -0,0 +1,9 @@
+// 10 Days Statistics > Day 2: Basic Probability
+
+/**
+ * Task:
+ *  In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most.
+ *
+ * Answer:
+ *  5/6
+ */
