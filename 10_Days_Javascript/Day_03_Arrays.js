7f6cf9ccd1a7b27e8c67d47b522f44c7cee2a18f
jyotil-raval <jyotil@outlook.com>
2026-01-24 14:26:15 +0530

    10 Days of JavaScript

--- 10_Days_Javascript/Day_03_Arrays.js
+++ 10_Days_Javascript/Day_03_Arrays.js
@@ -2,7 +2,7 @@ function getSecondLargest(numbs) {
   // Complete the function
   const max = Math.max(...numbs);
 
-  numbs = numbs.filter(num => num !== max);
+  numbs = numbs.filter((num) => num !== max);
 
   return Math.max(...numbs);
 }
